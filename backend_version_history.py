77373e7bd88288dcb569f0c11e4cbeebfcbffec5
Nipunn Koorapati <nipunn@convex.dev>
2023-07-13 04:05:08 +0000

    Update backend version history script

--- backend_version_history.py
+++ backend_version_history.py
@@ -1,18 +1,35 @@
+import os
 import subprocess
+from pprint import pprint
 
 from dotenv import load_dotenv
 
+from convex import ConvexClient
+
 load_dotenv(".env.local")
+CONVEX_URL = os.environ["CONVEX_URL"]
+convex_client = ConvexClient(CONVEX_URL)
+convex_client.set_debug(True)
+
 result = subprocess.run(
     ["big-brain-tool", "backend-version-history", "-n", "100"],
     capture_output=True,
     check=True,
 )
 
-lines = []
+rows = []
 for line in result.stdout.decode("utf-8").strip().split("\n"):
     parts = line.split("\t")
     assert len(parts) == 4
-    lines.append(parts)
+    assert parts[2] == ""
+    rows.append(
+        {
+            "pushDate": parts[0],
+            "version": parts[1],
+            "url": parts[3],
+        }
+    )
+rows = list(reversed(rows))
 
-print(lines)
+pprint(rows)
+convex_client.mutation("backend_version_history:upload", {"rows": rows})
