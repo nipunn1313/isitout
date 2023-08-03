99f4e71badedf83af36b646bef8b23998dea46d3
Tom Ballinger <tom@convex.dev>
2023-08-02 20:15:03 -0700

    Add client-side auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useMutation, useQuery } from "convex/react";
+import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 
 import { formatRFC7231, formatDistanceToNowStrict } from "date-fns";
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,47 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import {
+  Authenticated,
+  ConvexReactClient,
+  Unauthenticated,
+} from "convex/react";
 
+import { ConvexProviderWithAuth0 } from "convex/react-auth0";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
+
+import authConfig from "../convex/auth.config.js";
+const authInfo = authConfig.providers[0];
 const address = import.meta.env.VITE_CONVEX_URL;
 
 const convex = new ConvexReactClient(address);
 
+function LoginButton() {
+  const { loginWithRedirect } = useAuth0();
+  return <button onClick={() => loginWithRedirect()}>Log in</button>;
+}
+
 ReactDOM.render(
   <StrictMode>
-    <ConvexProvider client={convex}>
-      <App />
-    </ConvexProvider>
+    <Auth0Provider
+      domain={authInfo.domain}
+      clientId={authInfo.applicationID}
+      authorizationParams={{
+        redirect_uri:
+          typeof window === "undefined" ? undefined : window.location.origin,
+      }}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+    >
+      <ConvexProviderWithAuth0 client={convex}>
+        <Authenticated>
+          <App />
+        </Authenticated>
+        <Unauthenticated>
+          <LoginButton />
+        </Unauthenticated>
+      </ConvexProviderWithAuth0>
+    </Auth0Provider>
   </StrictMode>,
   document.getElementById("root")
 );
