cbc5311160edbeb4e7fd032933ab9b01fb637ead
Nipunn Koorapati <nipunn1313@gmail.com>
2024-08-14 10:54:41 -0700

    Switch to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import {
@@ -36,7 +36,7 @@ function LoginButton() {
   );
 }
 
-const container = document.getElementById("root");
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 root.render(
   <StrictMode>
