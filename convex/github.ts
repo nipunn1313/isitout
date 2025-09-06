85003d250544e37c2727d03838118c289fcf345b
Nipunn Koorapati <nipunn1313@gmail.com>
2025-09-05 17:03:08 -0700

    Add a couple more actions

--- convex/github.ts
+++ convex/github.ts
@@ -19,3 +19,37 @@ export const compareCommits = action({
     return comparison.data.status;
   },
 });
+
+export const getLatestConvexBackendRelease = action({
+  args: {},
+  handler: async (_ctx, _args) => {
+    const release = await octokit.repos.getLatestRelease({
+      owner: "get-convex",
+      repo: "convex-backend",
+    });
+    return release.data.tag_name;
+  },
+});
+
+export const getLatestConvexBackendContainer = action({
+  args: {},
+  handler: async (_ctx, _args) => {
+    const packages =
+      await octokit.packages.getAllPackageVersionsForPackageOwnedByOrg({
+        package_type: "container",
+        package_name: "convex-backend",
+        org: "get-convex",
+        per_page: 1,
+        page: 1,
+      });
+
+    if (packages.data.length === 0) {
+      throw new Error("No container packages found");
+    }
+
+    // Return the most recent package version name/tag
+    return (
+      packages.data[0].metadata?.container?.tags?.[0] || packages.data[0].name
+    );
+  },
+});
